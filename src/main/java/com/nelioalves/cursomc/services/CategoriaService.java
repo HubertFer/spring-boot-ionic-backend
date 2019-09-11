c130fad7f8f565ce6ef8c2a9fa274fc29033f9d2
HubertFer <humbertofernandezsosa@gmail.com>
2019-09-11 00:19:54 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,4 +58,9 @@ public class CategoriaService {
 				orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+	public Categoria fromCategoriaDTO (CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
 }
