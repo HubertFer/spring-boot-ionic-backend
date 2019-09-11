8457bc4ede2045eeb1f4edf12399a9d23a80a719
HubertFer <humbertofernandezsosa@gmail.com>
2019-09-11 16:20:31 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/nelioalves/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/StandardError.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class StandardError implements Serializable{
 
+	public class ValidationError {
+
+	}
+
 	private static final long serialVersionUID = 1L;
 	
 	private Integer status;
